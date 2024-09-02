d5d9e06784580cf6e5451fef071f3c3205729b42
okdaccsou <soulaymane.oc@gmail.com>
2024-09-02 22:19:14 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
 
-// telegram
 
-$chat_login = "1192619163";
-$bot_token = "2132471297:AAHG5kz-Vk3Bst19FXh-QbiWU9NwlTzJxyc";
 // email
-$to = 'email@yandex.com';
+$to = 'o30118026@gmail.com';
 
 
 ?>
